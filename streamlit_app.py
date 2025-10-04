7ad719c8e060bcfeadff2b9fbe272bda2a0ece53
azilizineb-wq <azilizineb@gmail.com>
2025-10-05 00:52:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="PapssImmo", page_icon="🏡", layout="wide")
+st.title("🏡 PapssImmo – test ok")
+st.write("Si tu vois ce message, le bon fichier s’exécute ✅")

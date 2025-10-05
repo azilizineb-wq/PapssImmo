4bb2b7e395d2f47f588d657cc9996c73e3179330
azilizineb-wq <azilizineb@gmail.com>
2025-10-05 23:51:54 +0200

    Create 1_Accueil.py

---
+++ pages/1_Accueil.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("👋 Bienvenue sur PapssImmo")
+
+st.write("""
+PapssImmo vous aide à trouver **la commune idéale** selon vos critères :  
+budget, surface, temps de trajet, qualité de vie et plus encore.
+""")
+
+st.markdown("### 🔧 Réglez vos critères à gauche")
+st.info("Puis explorez les recommandations ou la carte interactive.")

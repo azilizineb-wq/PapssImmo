5f47393f1d7e6c609337248c71d90a0a519d5726
azilizineb-wq <azilizineb@gmail.com>
2025-10-05 23:53:13 +0200

    Create 4_A_propos.py

---
+++ pages/4_A_propos.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("ℹ️ À propos")
+
+st.write("""
+**PapssImmo** est un projet d’aide à la décision immobilière en Île-de-France.  
+Il vise à rendre la recherche de logement plus fluide, personnalisée et interactive.
+""")
+
+st.markdown("📧 Contact : [contact@papssimmo.fr](mailto:contact@papssimmo.fr)")
